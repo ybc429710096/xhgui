95c667fec8ea2fbb3abc48da05f571a80081bc48
ybc <429710096@qq.com>
2017-09-20 15:10:26 +0800

    test

--- src/Xhgui/Middleware/Render.php
+++ src/Xhgui/Middleware/Render.php
@@ -9,8 +9,7 @@ class Xhgui_Middleware_Render extends Middleware
         $app = $this->app;
         $bool = strpos($_SERVER['REQUEST_URI'],'/login') === false;
         if (empty($_SESSION['username']) && $bool) {
-            var_dump($app->urlFor('login.index'));die;
-            $app->redirect($app->urlFor('login.index'));
+            header('Location:'.$app->urlFor('login.index'));
         }
 
         // Run the controller action/route function
