0388347e58ce8a5a0fb0bd6174dbaa6b092fb077
ybc <429710096@qq.com>
2017-09-20 15:05:38 +0800

    启用session

--- src/Xhgui/Middleware/Render.php
+++ src/Xhgui/Middleware/Render.php
@@ -8,7 +8,6 @@ class Xhgui_Middleware_Render extends Middleware
 
         $app = $this->app;
         $bool = strpos($_SERVER['REQUEST_URI'],'/login') === false;
-        var_dump($_SESSION['username']);
         if (empty($_SESSION['username']) && $bool) {
             $app->redirect($app->urlFor('login.index'));
         }
