291b9d8a7df16aa747d141931d58c9c5659c0396
ybc <429710096@qq.com>
2017-09-20 11:36:42 +0800

    test

--- src/Xhgui/Middleware/Render.php
+++ src/Xhgui/Middleware/Render.php
@@ -9,8 +9,7 @@ class Xhgui_Middleware_Render extends Middleware
         $app = $this->app;
 
         if (empty($_SESSION['username'])) {
-            // $app->redirect();
-            var_dump($app->urlFor('login.index'));
+            $app->redirect($app->urlFor('login.index'));
         }
 
         // Run the controller action/route function
